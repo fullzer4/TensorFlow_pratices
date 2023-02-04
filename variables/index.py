2524efac58f0a242fada6f43aff39bc9e4f130b8
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-04 07:40:13 -0300

    new

--- variables/index.py
+++ variables/index.py
@@ -2,10 +2,19 @@ import tensorflow as tf
 
 v = tf.Variable(0)
 
-def increment_by_one(v):
-        v = tf.add(v,1)
-        return v
+def soma(valor):
+        valor = tf.add(valor,1)
+        return valor
     
 for i in range(3):
-    v = increment_by_one(v)
+    v = soma(v)
     print(v)
+    
+"""
+Retorno:
+
+tf.Tensor(1, shape=(), dtype=int32)
+tf.Tensor(2, shape=(), dtype=int32)
+tf.Tensor(3, shape=(), dtype=int32)
+
+"""
